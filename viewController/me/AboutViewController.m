0e3d33cb9a2b7462406baa2fb48910c81a8bece4
lonelyprince <xueyanzhi123@gmail.com>
2017-09-29 10:28:44 +0800

    修改了图片

--- viewController/me/AboutViewController.m
+++ viewController/me/AboutViewController.m
@@ -39,9 +39,10 @@ self.tabBarController.tabBar.hidden = YES;
 {
     deviceString = [GGUtil deviceVersion];
     if ( [deviceString isEqualToString:@"iPhone4S"] || [deviceString isEqualToString:@"iPhone4"]) {
-        NSLog(@"此刻是5s和4s的大小");
+        NSLog(@"此刻是4s的大小");
         
-        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+//        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(89, 150, 148, 103)];
         
         //    imageView.image = [UIImage imageNamed:@"关于about"];
         imageView.image = [UIImage imageNamed:@"4关于"];
@@ -54,7 +55,7 @@ self.tabBarController.tabBar.hidden = YES;
         self.verLabShow.frame = CGRectMake((SCREEN_WIDTH - sizeOfVerLabShow.width)/2, 276, sizeOfVerLabShow.width, sizeOfVerLabShow.height);
         self.verLabShow.font = FONT(14);
         self.verLabShow.textColor = RGB(0x32, 0x32, 0x32);
-        [imageView addSubview:self.verLabShow];
+        [self.view addSubview:self.verLabShow];
         
         //新加版权信息
         self.copyrightLabShow = [[UILabel alloc]init];
@@ -63,12 +64,13 @@ self.tabBarController.tabBar.hidden = YES;
         self.copyrightLabShow.frame = CGRectMake((SCREEN_WIDTH - sizeOfVerLabShow1.width)/2, SCREEN_HEIGHT - 35, sizeOfVerLabShow1.width, sizeOfVerLabShow1.height);
         self.copyrightLabShow.font = FONT(13);
         self.copyrightLabShow.textColor = RGB(0x6c, 0x6c, 0x6c);
-        [imageView addSubview:self.copyrightLabShow];
+        [self.view addSubview:self.copyrightLabShow];
         
     }else if ([deviceString isEqualToString:@"iPhone5"] || [deviceString isEqualToString:@"iPhone5S"] ||[deviceString isEqualToString:@"iPhoneSE"] || [deviceString isEqualToString:@"iPhone5C"]   || [deviceString isEqualToString:@"iPhone Simulator"]) {
-        NSLog(@"此刻是6的大小");
+        NSLog(@"此刻是5的大小");
         
-        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+//        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(89, 170, 148, 103)];
         
         //    imageView.image = [UIImage imageNamed:@"关于about"];
         imageView.image = [UIImage imageNamed:@"关于"];
@@ -81,7 +83,7 @@ self.tabBarController.tabBar.hidden = YES;
         self.verLabShow.frame = CGRectMake((SCREEN_WIDTH - sizeOfVerLabShow.width)/2, 300, sizeOfVerLabShow.width, sizeOfVerLabShow.height);
         self.verLabShow.font = FONT(15);
         self.verLabShow.textColor = RGB(0x32, 0x32, 0x32);
-        [imageView addSubview:self.verLabShow];
+        [self.view addSubview:self.verLabShow];
         
         
         //新加版权信息
@@ -91,12 +93,13 @@ self.tabBarController.tabBar.hidden = YES;
         self.copyrightLabShow.frame = CGRectMake((SCREEN_WIDTH - sizeOfVerLabShow1.width)/2, SCREEN_HEIGHT - 32, sizeOfVerLabShow1.width, sizeOfVerLabShow1.height);
         self.copyrightLabShow.font = FONT(13);
         self.copyrightLabShow.textColor = RGB(0x6c, 0x6c, 0x6c);
-        [imageView addSubview:self.copyrightLabShow];
+        [self.view addSubview:self.copyrightLabShow];
     }
     else if ([deviceString isEqualToString:@"iPhone6"] || [deviceString isEqualToString:@"iPhone6S"] || [deviceString isEqualToString:@"iPhone7"]  ) {
         NSLog(@"此刻是6的大小");
         
-        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+//        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(117, 210, 148, 103)];
         
         //    imageView.image = [UIImage imageNamed:@"关于about"];
         imageView.image = [UIImage imageNamed:@"关于"];
@@ -110,7 +113,8 @@ self.tabBarController.tabBar.hidden = YES;
         self.verLabShow.frame = CGRectMake((SCREEN_WIDTH - sizeOfVerLabShow.width)/2, 356, sizeOfVerLabShow.width, sizeOfVerLabShow.height);
         self.verLabShow.font = FONT(16);
         self.verLabShow.textColor = RGB(0x32, 0x32, 0x32);
-        [imageView addSubview:self.verLabShow];
+//        [imageView addSubview:self.verLabShow];
+        [self.view addSubview:self.verLabShow];
         
         //新加版权信息
         self.copyrightLabShow = [[UILabel alloc]init];
@@ -119,13 +123,14 @@ self.tabBarController.tabBar.hidden = YES;
         self.copyrightLabShow.frame = CGRectMake((SCREEN_WIDTH - sizeOfVerLabShow1.width)/2, SCREEN_HEIGHT - 35, sizeOfVerLabShow1.width, sizeOfVerLabShow1.height);
         self.copyrightLabShow.font = FONT(13);
         self.copyrightLabShow.textColor = RGB(0x6c, 0x6c, 0x6c);
-        [imageView addSubview:self.copyrightLabShow];
+        [self.view addSubview:self.copyrightLabShow];
         
     }else if ([deviceString isEqualToString:@"iPhone6 Plus"] || [deviceString isEqualToString:@"iPhone6S Plus"] || [deviceString isEqualToString:@"iPhone7 Plus"] ) {
         NSLog(@"此刻是6 plus的大小");
         
         
-        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, -7, SCREEN_WIDTH, SCREEN_HEIGHT+7)];
+//        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, -7, SCREEN_WIDTH, SCREEN_HEIGHT+7)];
+        UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(135, 238, 148, 103)];
         
         //    imageView.image = [UIImage imageNamed:@"关于about"];
         imageView.image = [UIImage imageNamed:@"关于"];
@@ -138,7 +143,7 @@ self.tabBarController.tabBar.hidden = YES;
         self.verLabShow.frame = CGRectMake((SCREEN_WIDTH - sizeOfVerLabShow.width)/2, 390, sizeOfVerLabShow.width, sizeOfVerLabShow.height);
         self.verLabShow.font = FONT(18);
         self.verLabShow.textColor = RGB(0x32, 0x32, 0x32);
-        [imageView addSubview:self.verLabShow];
+        [self.view addSubview:self.verLabShow];
         
         //新加版权信息
         self.copyrightLabShow = [[UILabel alloc]init];
@@ -147,7 +152,7 @@ self.tabBarController.tabBar.hidden = YES;
         self.copyrightLabShow.frame = CGRectMake((SCREEN_WIDTH - sizeOfVerLabShow1.width)/2, SCREEN_HEIGHT - 35, sizeOfVerLabShow1.width, sizeOfVerLabShow1.height);
         self.copyrightLabShow.font = FONT(13);
         self.copyrightLabShow.textColor = RGB(0x6c, 0x6c, 0x6c);
-        [imageView addSubview:self.copyrightLabShow];
+        [self.view addSubview:self.copyrightLabShow];
         
     }
     
